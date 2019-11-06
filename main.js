3739757294de8da526f50b3142c53713b7200a65
vlad kuznetsov <vlad.y.kuznetsov@gmail.com>
2019-11-06 03:38:41 -0500

    Removed smooth scrolling feature from carousel anchor

--- main.js
+++ main.js
@@ -2,7 +2,37 @@ $(document).ready(function () {
 
   //$('#animated').hide().fadeIn(4000)
 
-  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  document.querySelectorAll('a[href^="#home"]').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+          e.preventDefault();
+
+          document.querySelector(this.getAttribute('href')).scrollIntoView({
+              behavior: 'smooth'
+          });
+      });
+  });
+
+  document.querySelectorAll('a[href^="#about"]').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+          e.preventDefault();
+
+          document.querySelector(this.getAttribute('href')).scrollIntoView({
+              behavior: 'smooth'
+          });
+      });
+  });
+
+  document.querySelectorAll('a[href^="#projects"]').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+          e.preventDefault();
+
+          document.querySelector(this.getAttribute('href')).scrollIntoView({
+              behavior: 'smooth'
+          });
+      });
+  });
+
+  document.querySelectorAll('a[href^="#contact"]').forEach(anchor => {
       anchor.addEventListener('click', function (e) {
           e.preventDefault();
 
