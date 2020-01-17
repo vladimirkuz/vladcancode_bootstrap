f623c8e0085b1b9dcc6d864e85ea432ef7c98546
vlad kuznetsov <vlad.y.kuznetsov@gmail.com>
2020-01-16 21:32:14 -0500

    Added a submit form

--- main.js
+++ main.js
@@ -2,6 +2,20 @@ $(document).ready(function () {
 
   //$('#animated').hide().fadeIn(4000)
 
+  /*$('#submit-form').submit(function(e){
+    e.preventDefault();
+  });
+*/
+
+ $('#form').submit( (e) => {
+  e.preventDefault();
+  $.ajax({
+      url:'http://localhost:4001/send-email',
+      type:'post',
+      data:$('#form').serialize()
+  }).toggle(400);
+})
+
   document.querySelectorAll('a[href^="#home"]').forEach(anchor => {
       anchor.addEventListener('click', function (e) {
           e.preventDefault();
